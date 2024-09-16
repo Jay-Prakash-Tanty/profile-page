721bc2b97c756e21cdc982919b5a7be707e7031e
dharmendra007 <mahantas025@gmail.com>
2024-09-16 10:08:50 +0530

    feat(auth): add Zod schemas for signup and login with TypeScript types

--- src/components/(auth)/authSchema.ts
+++ src/components/(auth)/authSchema.ts
@@ -1,15 +1,21 @@
 import { z } from 'zod';
 
 /*
-password must contain atleast 8 characters , a capital letter , a small letter and a special character
+password must contain atleast 8 characters , a uppercase letter , a lowercase letter and a special character
 */
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+
+/*
+uppercase, lowercase, space, hyphen(-), apostrophe (') are allowed
+*/
+const fullNameRegex = /^[a-zA-Z\s'-]{3,50}$/;
 
 const signupSchema = z.object({
   fullName: z
     .string()
-    .min(1, "Full name is required")
-    .max(100, "Full name must be less than 100 characters"),
+    .min(3, "Full name is required")
+    .max(50, "Full name must be less than 50 characters")
+    .regex(fullNameRegex, "only uppercase, lowercase, space, hyphen(-), apostrophe (') are allowed"),
   email: z
     .string()
     .email("Invalid email address")
@@ -17,7 +23,7 @@ const signupSchema = z.object({
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
-    .regex(passwordRegex, "Password must contain at least one uppercase letter, one lowercase letter, and one special character")
+    .regex(passwordRegex, "Password must contain at least a uppercase letter, a lowercase letter, and a special character")
 });
 
 const loginSchema = z.object({

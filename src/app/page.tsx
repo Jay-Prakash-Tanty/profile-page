ac42e8ec233ecf5f2a6277dc0e796dd2f107d203
psonakshi <sonakshii1806@gmail.com>
2024-10-03 12:48:53 +0530

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
 import MainLogin from "@/components/(auth)/MainLogin";
-
+import Carousel from "@/components/carousal/Carousal";
+import ResponsiveAppBar from "@/components/navbar/ResponsiveAppBar";
 export default function Home() {
+const images = [
+  {
+    src: '/images/image1.jpg',
+    caption: 'Explore all the clubs \n of your college',
+  },
+  {
+    src: '/images/image2.jpg',
+    caption: 'Explore all the clubs of your college',
+  },
+  {
+    src: '/images/image3.jpg',
+    caption: 'Explore all the clubs of your college',
+  },
+];
+
   return (
     <>
-      <MainLogin/>
+      <div className="overflow-hidden">
+      <ResponsiveAppBar />
+      <div style={{ width: '100%' }}>
+        <Carousel
+          imagesArray={images}
+          interval={5000}
+        >
+        </Carousel>
+      </div>
+    </div>
     </>
   );
 }

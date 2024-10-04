da8e27bd0876461cf504cb9d88e50a80bb190bb7
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-10-04 14:49:17 +0530

    Completed

---
+++ src/EventsAndNews/EventsAndNews.tsx
@@ -0,0 +1,58 @@
+import { Box, Typography, Grid } from "@mui/material";
+
+import Left from "./Components/Left";
+import Right from "./Components/Right";
+import Middle from "./Components/Middle";
+
+export default function EventsAndNews() {
+  return (
+    <div className="min-h-[100vh] w-[100vw] bg-[#1E1E1E] flex flex-col gap-20 pb-10 items-center justify-center gap-[40px]">
+      <Box sx={{display:"flex",flexDirection:"column", alignItems:"center"}} >
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontWeight: "600",
+            fontSize: {
+              xs:"24px",
+              md:"36px",
+
+            },
+            lineHeight: "54px",
+            paddingTop: "97px",
+          }}
+        >
+          Events and News
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: "600",
+          
+            
+            fontSize: {
+              xs:"16px",
+              md:"20px"
+            },
+            
+            lineHeight: "30px",
+            color: "#757575",
+            textAlign: "center",
+            paddingBottom:"11px"
+           
+            
+          }}
+        >
+          Stay Updated with the Latest Events and News
+        </Typography>
+      </Box>
+      <Box >
+        <Grid container direction="row" justifyContent="center" alignItems="center" gap="20px">
+          <Grid item>
+            <Left />
+          </Grid>
+          <Grid item ><Middle/></Grid>
+          <Grid item ><Right/></Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}

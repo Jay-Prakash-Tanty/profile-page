1cefa796ad4b1e83cf169724185bd8c3cd805e27
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-10-04 21:46:41 +0530

    Lowered The button , fixed bg color

--- src/EventsAndNews/EventsAndNews.tsx
+++ src/EventsAndNews/EventsAndNews.tsx
@@ -1,21 +1,32 @@
 import { Box, Typography, Grid } from "@mui/material";
-
 import Left from "./Components/Left";
 import Right from "./Components/Right";
 import Middle from "./Components/Middle";
 
 export default function EventsAndNews() {
   return (
-    <div className="h-[100vh] w-[100vw] bg-[#1E1E1E]  flex-col items-center justify-center ">
-      <Box sx={{display:"flex",flexDirection:"column", alignItems:"center"}} >
+    <Box
+      sx={{
+        minHeight: "100vh",
+        width: "100vw",
+        paddingBottom:"10px",
+        paddingTop:"10px",
+        backgroundColor: "#1E1E1E",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap:"60px"
+      }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
         <Typography
           sx={{
             textAlign: "center",
             fontWeight: "600",
             fontSize: {
-              xs:"24px",
-              md:"36px",
-
+              xs: "24px", 
+              md: "36px",
             },
             lineHeight: "54px",
             paddingTop: "10px",
@@ -26,33 +37,32 @@ export default function EventsAndNews() {
         <Typography
           sx={{
             fontWeight: "600",
-          
-            
             fontSize: {
-              xs:"16px",
-              md:"20px"
+              xs: "16px",
+              md: "20px",
             },
-            
             lineHeight: "30px",
             color: "#757575",
             textAlign: "center",
-            paddingBottom:"11px"
-           
-            
+            paddingBottom: "11px",
           }}
         >
           Stay Updated with the Latest Events and News
         </Typography>
       </Box>
-      <Box >
+      <Box>
         <Grid container direction="row" justifyContent="center" alignItems="center" gap="20px">
           <Grid item>
             <Left />
           </Grid>
-          <Grid item ><Middle/></Grid>
-          <Grid item ><Right/></Grid>
+          <Grid item>
+            <Middle />
+          </Grid>
+          <Grid item>
+            <Right />
+          </Grid>
         </Grid>
       </Box>
-    </div>
+    </Box>
   );
 }

19e873161c47bcba9d0104b4d40294c9779b5d04
sumanpatra006 <ksumanpatra06@gmail.com>
2024-10-08 07:09:50 +0530

    update authtype for signup and signin methods & responsiveness of all the components

--- src/components/(auth)/MainLogin.tsx
+++ src/components/(auth)/MainLogin.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Login from './Login'
-import LoginBox from './LoginBox'
-import { Box } from '@mui/material'
+import React from 'react';
+import Login from './Login';
+import LoginBox from './LoginBox';
+import { Box } from '@mui/material';
+import LoginDetails from './LoginDetails';
 
-function MainLogin() {
+function MainLogin({ authType }: { authType: 'sign-in' | 'sign-up' }) {
   return (
     <Box bgcolor='#1E1E1E'>
-      <Login/>
-      <LoginBox/>
+      <Login />
+      {/* Pass authType to LoginBox */}
+      <LoginBox authType={authType} />
     </Box>
-  )
+  );
 }
 
-export default MainLogin
+export default MainLogin;
